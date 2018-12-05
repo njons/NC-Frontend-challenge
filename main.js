50e417239b1bd599c94e2094ecef63032ecf4eaf
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-12-05 22:15:10 +0100

    adds event listener to input and fitText function

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 console.log("this is main.js");
-
+const inputDiv = document.querySelector("#input__text");
 const outputDiv = document.querySelector("#output__div");
-const inputText = document.querySelector("#input__text");
+const outputText = document.querySelector("#output__text");
 const slider = document.querySelector("#slider");
 
 slider.addEventListener("change", event => {
   console.log("you have changed the slider");
+  let sliderValue = slider.value;
+  console.log("this is the slider value:", sliderValue);
+  // use value from slide to calculate the width outputDiv
+  fitText();
 });
+
+inputDiv.addEventListener("keyup", event => {
+  console.log("you have keyed up");
+  let text = inputDiv.value;
+  console.log("this is text:", text);
+  // use text input to calculate the width of the output text
+  fitText();
+});
+
+const fitText = outputDiv => {
+  console.log(
+    "this is the clientWidth of the text element :",
+    outputText.clientWidth
+  );
+  console.log(
+    "this is the clientWidth of the button element :",
+    outputDiv.clientWidth
+  );
+};
