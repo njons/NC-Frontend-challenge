e0f5e7210e8227a62f7e97f2fa290a42e4169f9e
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-12-05 23:26:32 +0100

    adds functionality to calculate and set width of the outputDiv

--- main.js
+++ main.js
@@ -4,29 +4,57 @@ const outputDiv = document.querySelector("#output__div");
 const outputText = document.querySelector("#output__text");
 const slider = document.querySelector("#slider");
 
+const convertVwToPx = vw => {
+  console.log("you have reached convertVwToPx");
+  // const container = document.querySelector("#output__container");
+  console.log("this is the vw coming in :", vw);
+  console.log("this is the window.innerWidth:", window.innerWidth);
+  let windowWidth = (window.innerWidth * 0.01).toFixed(3);
+  console.log("this is the window width:", windowWidth);
+  console.log("this is the window width * vw:", windowWidth * vw);
+  return windowWidth * vw;
+};
+
+const setOutputDivWidth = sliderValue => {
+  console.log("you have reached setOutputDivWidth");
+  console.log("this is the slider value:", sliderValue);
+  let calcWidth = convertVwToPx(100 * sliderValue);
+  console.log("this is the calcWidth value:", calcWidth);
+  outputDiv.style.width = calcWidth - 173 + "px";
+};
+
+const fitText = outputDiv => {
+  console.log("you have reached fitText");
+  // use value from slide to calculate the width outputDiv
+
+  console.log(
+    "this is the clientWidth of the text element :",
+    outputText.clientWidth
+  );
+
+  console.log(
+    "this is the clientWidth of the button element :",
+    outputDiv.clientWidth
+  );
+
+  console.log(
+    "this is the getBoundingClientRect of the button element :",
+    outputDiv.getBoundingClientRect().width
+  );
+};
+
 slider.addEventListener("change", event => {
   console.log("you have changed the slider");
   let sliderValue = slider.value;
-  console.log("this is the slider value:", sliderValue);
-  // use value from slide to calculate the width outputDiv
-  fitText();
+  setOutputDivWidth(sliderValue);
+  fitText(outputDiv);
 });
 
 inputDiv.addEventListener("keyup", event => {
   console.log("you have keyed up");
   let text = inputDiv.value;
   console.log("this is text:", text);
+  outputText.textContent = text;
   // use text input to calculate the width of the output text
-  fitText();
+  fitText(outputDiv);
 });
-
-const fitText = outputDiv => {
-  console.log(
-    "this is the clientWidth of the text element :",
-    outputText.clientWidth
-  );
-  console.log(
-    "this is the clientWidth of the button element :",
-    outputDiv.clientWidth
-  );
-};
