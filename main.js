bb2887768641a525d8c839b4f81b42c9c3f4cf50
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-12-05 21:58:37 +0100

    adds event listener to slider

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("this is main.js");
+
+const outputDiv = document.querySelector("#output__div");
+const inputText = document.querySelector("#input__text");
+const slider = document.querySelector("#slider");
+
+slider.addEventListener("change", event => {
+  console.log("you have changed the slider");
+});
